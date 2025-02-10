0b1a557e0812e90af36de72e9f45210ac79e40b3
flyingking888 <jeremyfeiwang@outlook.com>
2025-02-10 13:57:34 -0500

    2-8

--- Flagler Student/HomeViewController.swift
+++ Flagler Student/HomeViewController.swift
@@ -7,13 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func submitButton(_ sender: UIButton) {
+        
+        messageLabel.text = "Hello world! Thank you for your submission!"
+        
+    }
+    
 }
 
