9f8b43664a9351f83246a864e8ab72c4d6166362
flyingking888 <jeremyfeiwang@outlook.com>
2025-02-12 16:38:53 -0500

    2-12

--- Flagler Student/HomeViewController.swift
+++ Flagler Student/HomeViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import MapKit
 
 class HomeViewController: UIViewController {
     
@@ -16,9 +17,31 @@ class HomeViewController: UIViewController {
     
     @IBOutlet weak var lastNameTextField: UITextField!
     
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let flaglerCollegeLocation = CLLocationCoordinate2D(latitude: 29.8928, longitude: -81.3150)
+        
+        /*
+        let region = MKCoordinateRegion(center: flaglerCollegeLocation, latitudinalMeters: 10000, longitudinalMeters: 10000)f
+        */
+        
+        let region = MKCoordinateRegion(center: flaglerCollegeLocation, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        mapView.setRegion(region, animated: true)
+        
+        //Let's add a pin for Flagler
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = flaglerCollegeLocation
+        annotation.title = "Flagler College"
+        annotation.subtitle = "The most beautiful college"
+        mapView.addAnnotation(annotation)
+               
+        
     }
 
     
@@ -26,15 +49,33 @@ class HomeViewController: UIViewController {
         
         //Use the same object-oriented programming techniques
         //Access the members of the instantiated objects
+        //Optionals!
         
+        /*
         let firstName = firstNameTextField.text ?? ""
         let lastName = lastNameTextField.text ?? ""
+         */
+        
+        guard let firstName = firstNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines), !firstName.isEmpty,
+             let lastName = lastNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines), !lastName.isEmpty
+        else {
+            return showAlert(message: "Both first name and last name are requried!")
+        }
+        
         
         //Interpolation is preferred in Swift!
         messageLabel.text = "Welcome to Flagler,\(firstName) \(lastName)! Flagler is beautiful!"
         
-        
     }
+    
+    //Outside the button function, we'll add a customer function
+    func showAlert(message: String) -> Void {
+        let alertController = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(alertAction)
+        present(alertController, animated: true)
+    }
+
     
     
 }
