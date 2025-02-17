7b62286e7bdfc307dc566fed5bf3ae566fa48ef8
flyingking888 <jeremyfeiwang@outlook.com>
2025-02-17 12:28:26 -0500

    2-12

--- Flagler Student/HomeViewController.swift
+++ Flagler Student/HomeViewController.swift
@@ -25,16 +25,31 @@ class HomeViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         let flaglerCollegeLocation = CLLocationCoordinate2D(latitude: 29.8928, longitude: -81.3150)
+        /*
+         latitude: 29.8928 → It’s 29.89° North of the Equator.
+         longitude: -81.3150 → It’s 81.31° West of the Prime Meridian.
+         */
         
+        // Set zoom level (region span)
         /*
         let region = MKCoordinateRegion(center: flaglerCollegeLocation, latitudinalMeters: 10000, longitudinalMeters: 10000)f
         */
         
         let region = MKCoordinateRegion(center: flaglerCollegeLocation, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
         
+        /*
+         latitudeDelta / longitudeDelta    Zoom Level    Example Usage
+         0.01 / 0.01    Very zoomed-in    City block level (detailed)
+         0.05 / 0.05    Medium zoom    Covers a small town or district
+         0.1 / 0.1    Wider zoom    Covers an entire city
+         1.0 / 1.0    Far zoomed-out    Covers a large region or state
+         10.0 / 10.0    Extremely zoomed-out    Covers multiple states or countries
+         */
+        
+        // Apply region to the map
         mapView.setRegion(region, animated: true)
         
-        //Let's add a pin for Flagler
+        //Let's add a pin (annotation) for Flagler
         let annotation = MKPointAnnotation()
         annotation.coordinate = flaglerCollegeLocation
         annotation.title = "Flagler College"
@@ -61,8 +76,7 @@ class HomeViewController: UIViewController {
         else {
             return showAlert(message: "Both first name and last name are requried!")
         }
-        
-        
+                
         //Interpolation is preferred in Swift!
         messageLabel.text = "Welcome to Flagler,\(firstName) \(lastName)! Flagler is beautiful!"
         
