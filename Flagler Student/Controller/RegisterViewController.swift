f2d3851de9bb5c1656e4980da2bbb773dc80f7af
flyingking888 <jeremyfeiwang@outlook.com>
2025-04-07 18:07:36 -0400

    Firebase updates

--- Flagler Student/Controller/RegisterViewController.swift
+++ Flagler Student/Controller/RegisterViewController.swift
@@ -6,24 +6,58 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        messageLabel.text = ""
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func registerPressed(_ sender: UIButton) {
+        
+        // ==================Use Input Validation===============
+        // check for nil, empty ("")
+        guard let email = emailTextField.text, !email.isEmpty else {
+            messageLabel.text = "Please enter your email."
+            return
+        }
+        
+        guard let password = passwordTextField.text, !password.isEmpty else {
+            messageLabel.text = "Please enter your password."
+            return
+        }
+        
+        //auth() is an example of singleton reference, which is shared by the whole app.
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let e = error {
+                print(e)
+            } else {
+                //Navigate to the TabBarController
+                
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                if let tabBarController = storyboard.instantiateViewController(withIdentifier: "MainTabBarController") as? UITabBarController {
+                    // Optional: make the transition fullscreen
+                    tabBarController.modalPresentationStyle = .fullScreen
+                    self.present(tabBarController, animated: true, completion: nil)
+                }
+                
+            }
+        }
+        
+        
+        
     }
-    */
+    
 
 }
